7e9c202475805c8b321e672fa1c82260522d8ab8
nurahmetemin <nurahmedemin@gmail.com>
2022-11-24 19:37:34 -0800

    added ProjectServiceImpl.java and overoad methods

--- src/main/java/com/cydeo/dto/ProjectDTO.java
+++ src/main/java/com/cydeo/dto/ProjectDTO.java
@@ -2,9 +2,15 @@ package com.cydeo.dto;
 
 import com.cydeo.entity.User;
 import com.cydeo.enums.Status;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class ProjectDTO {
 
     private String projectName;
