77315ddd03d1c670671e7360a0b0e73b08fc9f4f
nurahmetemin <nurahmedemin@gmail.com>
2022-11-24 20:13:28 -0800

    binded object with the view

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -1,7 +1,11 @@
 package com.cydeo.controller;
 
 
+import com.cydeo.dto.ProjectDTO;
+import com.cydeo.service.ProjectService;
+import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,10 +13,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/project")
 public class ProjectController {
 
+    ProjectService projectService;
+    UserService userService;
+
+    public ProjectController(ProjectService projectService, UserService userService) {
+        this.projectService = projectService;
+        this.userService = userService;
+    }
 
     @GetMapping("/create")
-    public String creatProject(){
+    public String creatProject(Model model){
 
+        model.addAttribute("project", new ProjectDTO());
+        model.addAttribute("projects", projectService.findAll()  );
+        model.addAttribute("managers", userService.findAll()    );
 
         return "project/create";
     }
