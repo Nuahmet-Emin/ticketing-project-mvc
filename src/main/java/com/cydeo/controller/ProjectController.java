485e1db591b358170144099767e493132dedaa6e
nurahmetemin <nurahmedemin@gmail.com>
2022-11-26 22:49:57 -0800

    fixed errors

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -1,13 +1,13 @@
 package com.cydeo.controller;
 
-
 import com.cydeo.dto.ProjectDTO;
+import com.cydeo.enums.Status;
 import com.cydeo.service.ProjectService;
 import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -24,21 +24,22 @@ public class ProjectController {
     }
 
     @GetMapping("/create")
-    public String creatProject(Model model){
+    public String createProject(Model model){
 
-        model.addAttribute("project", new ProjectDTO());
-        model.addAttribute("projects", projectService.findAll()  );
-        model.addAttribute("managers", userService.findAll()    );
+        model.addAttribute("project",new ProjectDTO());
+        model.addAttribute("projects",projectService.findAll());
+        model.addAttribute("managers",userService.findAll());
 
-        return "project/create";
+
+        return "/project/create";
     }
 
     @PostMapping("/create")
-    //public String insertProject(@ModelAttribute("project") ProjectDTO project, Model model){ same as below
     public String insertProject(ProjectDTO project){
+
         projectService.save(project);
 
-        return "redirect:project/create";
+        return "redirect:/project/create";
     }
 
 
