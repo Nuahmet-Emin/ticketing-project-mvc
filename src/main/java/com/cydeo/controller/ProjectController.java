274e586bed4a6f5f573c01869e40002993ffbee3
nurahmetemin <nurahmedemin@gmail.com>
2022-11-29 17:50:45 -0800

    implemented updated

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -6,10 +6,7 @@ import com.cydeo.service.ProjectService;
 import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/project")
@@ -59,12 +56,21 @@ public class ProjectController {
     }
 
     @GetMapping("/update/{projectCode}")
-    public String updateProject(@PathVariable("projectCode") String projectCode, Model model){
+    public String editProject(@PathVariable("projectCode") String projectCode, Model model){
 
         model.addAttribute("project",projectService.findById(projectCode));
         model.addAttribute("projects",projectService.findAll());
         model.addAttribute("managers",userService.findManagers());
         return "/project/update";
+
+    }
+
+    @PostMapping("/update")
+    public String updateProject(ProjectDTO project){
+
+      projectService.update(project);
+
+        return "redirect:/project/create";
 
     }
 
